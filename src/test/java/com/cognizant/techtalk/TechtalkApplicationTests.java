5dd84a9fa2304b9ed1942bc3706824f5c2c83c96
Prakash Kaliyannan <prakash.kaliyannan@cognizant.com>
2022-03-22 21:43:10 +0530

    TECHTALK - Initial setup

---
+++ src/test/java/com/cognizant/techtalk/TechtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.techtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
