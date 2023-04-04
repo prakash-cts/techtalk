141d9fecf1a544aac5978485dea3e47f14fe4931
Prakash Kaliyannan <prakash.kaliyannan@cognizant.com>
2023-04-04 17:48:20 +0530

    Sarmi commit

--- src/main/java/com/cognizant/techtalk/sarmi/SarmiMain.java
+++ src/main/java/com/cognizant/techtalk/sarmi/SarmiMain.java
@@ -7,13 +7,9 @@ import java.util.*;
  */
 public class SarmiMain {
     public static void main(String[] args) {
-//        List<Integer> indexList1 = getIndexOfSwappedLetters("Helloh worldw",'H');
-//        List<Integer> indexList2 = getIndexOfSwappedLetters("Helloh worldw",'W');
-//        System.out.println(indexList1);
-//        System.out.println(indexList2);
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the position ");
-        int pos = in.nextInt();
+        System.out.println("Number of swaps ");
+        int pos2 = in.nextInt();
         System.out.println("Enter the character ");
         String input = in.nextLine();
         String input2 = in.nextLine();
@@ -23,22 +19,16 @@ public class SarmiMain {
         String input1 = in.nextLine();
         List<Integer> indexList1 = getIndexOfSwappedLetters(input1,character1);
         List<Integer> indexList2 = getIndexOfSwappedLetters(input1,character2);
-//        System.out.println(indexList1);
-//        System.out.println(indexList2);
-//        System.out.println(character1);
-//        System.out.println(pos);
-//        System.out.println(input);
-//        System.out.println(input2);
-        input1 = replaceSubString(input1, character2,indexList1);
-        input1 = replaceSubString(input1, character1,indexList2);
+
+        input1 = replaceSubString(input1, character2,indexList1,pos2);
+        input1 = replaceSubString(input1, character1,indexList2,pos2);
+        System.out.println(input1);
     }
-    private static String replaceSubString(String str,char character, List<Integer>  posList){
-//        String str = "Techie Delight";
-//        char ch = '_';
-//        int pos = 1;
-        System.out.println(str);
-        System.out.println(character);
+    private static String replaceSubString(String str,char character, List<Integer>  posList, int pos2){
         System.out.println(posList);
+        Collections.sort(posList);
+        System.out.println(posList);
+        int count = 0;
         for(int i:posList){
             int pos = i;
             StringBuilder sb = new StringBuilder(str);
@@ -48,16 +38,15 @@ public class SarmiMain {
                 char c= Character.toLowerCase(character);
                 sb.setCharAt(pos, c);
             }
-            // replace character at the specified position
 
             str = sb.toString();
 
-            // print the modified string
-            System.out.println(str);
-
+            count ++;
+            if(count==pos2){
+                break;
+            }
         }
         return str;
-
     }
 
     private static List<Integer> getIndexOfSwappedLetters(String str, char character) {
@@ -65,7 +54,6 @@ public class SarmiMain {
             System.out.println("Provide upper case only");
             System.exit(0);
         }
-//        String characterString = String.valueOf(character);
         List<Integer> indexList = new ArrayList<>();
         List<Character> charList = new ArrayList<>();
         charList.add(character);
@@ -74,12 +62,10 @@ public class SarmiMain {
         for(char character1:charList){
             int index = s1.indexOf(character1);
             while (index != -1) {
-//                System.out.println(index);
                 indexList.add(index);
                 index = s1.indexOf(character1, index + 1);
             }
         }
-
         return indexList;
     }
 }
